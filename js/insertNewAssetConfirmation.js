e45e43352629923155d9325e73fa9b5994c120a0
Cameron Ellis <cameronellis71@gmail.com>
2017-10-27 14:07:28 -0700

    Added logic for dropdown button in Insert New Item Success Confirmation web page

---
+++ js/insertNewAssetConfirmation.js
@@ -0,0 +1,17 @@
+// code to implement accordion behavior for dropdown menu
+// Link to code: https://www.w3schools.com/howto/howto_js_accordion.asp
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  }
+}
