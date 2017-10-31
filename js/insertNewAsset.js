6960715bd6229f2e77867495baa671372134987d
Cameron Ellis <cameronellis71@gmail.com>
2017-10-31 14:28:49 -0700

    Adding logic to enforce the completion of forms

--- js/insertNewAsset.js
+++ js/insertNewAsset.js
@@ -7,3 +7,37 @@ for(let year = start ; year >=end; year--){
   options += "<option>"+ year +"</option>";
 }
 document.getElementById("year").innerHTML = options;
+
+$(document).ready(function(){
+	// When the submit button is clicked
+	$("#submitButton").click(function(){
+		let itemNameField = $("#itemNameField").val();
+		let itemPriceField = $('#itemPriceField').val();
+
+		console.log("itemNameField: " + itemNameField);
+		console.log("itemPriceField: " + itemPriceField);
+
+		// check if all forms filled out here...
+
+		// If an item name is not given
+		if(itemNameField == ""){
+			// Give input box a red border
+			$("#itemNameSection").addClass("has-error");
+			// Error Message
+			$("#itemNameError").text("Please specify an item name");
+		}
+		else{
+			$("#itemNameSection").addClass("has-success");
+		}
+
+		if(itemPriceField == ""){
+			// Give input box a red border
+			$('#itemPriceSection').addClass("has-error");
+			// Error message
+			$("#priceValueError").text("Please specify the item's price");
+		}
+		else{
+			$('#itemPriceSection').addClass("has-success");
+		}
+	});
+});
