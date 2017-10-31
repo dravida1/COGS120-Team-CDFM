a09cefe4854cf4d21c9182ef064b1c401a6483eb
Cameron Ellis <cameronellis71@gmail.com>
2017-10-31 15:22:57 -0700

    Added check to see if everything is filled in

--- js/insertNewAsset.js
+++ js/insertNewAsset.js
@@ -18,81 +18,89 @@ $(document).ready(function(){
 		let itemReceiptField = $("#itemReceiptField").val();
 		let itemManufacturerField = $("#itemManufacturerField").val();
 
-		console.log("itemNameField: " + itemNameField);
-		console.log("itemPriceField: " + itemPriceField);
-		console.log("itemYearField: " + itemYearField);
-		console.log("itemPictureField: " + itemPictureField);
-		console.log("itemReceiptField: " + itemReceiptField);
-		console.log("itemManufacturerField: " + itemManufacturerField);
+		// console.log("itemNameField: " + itemNameField);
+		// console.log("itemPriceField: " + itemPriceField);
+		// console.log("itemYearField: " + itemYearField);
+		// console.log("itemPictureField: " + itemPictureField);
+		// console.log("itemReceiptField: " + itemReceiptField);
+		// console.log("itemManufacturerField: " + itemManufacturerField);
 
-		// check if all forms filled out here...
+		// check if all mandatory forms are filled in...
+		if(itemNameField != "" && itemPriceField != "" && itemYearField != "" 
+			&& itemPictureField != "" && itemReceiptField != "" && itemManufacturerField != ""){
+			console.log("Everything's filled in!");
 
-		// If an item name is not given
-		if(itemNameField == ""){
-			// Give input box a red border
-			$("#itemNameSection").addClass("has-error");
-			// Error Message
-			$("#itemNameError").text("Please provide an item name");
 		}
 		else{
-			$("#itemNameSection").addClass("has-success");
+			// If an item name is not given
+			if(itemNameField == ""){
+				// Give input box a red border
+				$("#itemNameSection").addClass("has-error");
+				// Error Message
+				$("#itemNameError").text("Please provide an item name");
+			}
+			else{
+				$("#itemNameSection").addClass("has-success");
+			}
+
+			// if an item price is not given
+			if(itemPriceField == ""){
+				// Give input box a red border
+				$('#itemPriceSection').addClass("has-error");
+				// Error message
+				$("#priceValueError").text("Please provide the item's price");
+			}
+			else{
+				$('#itemPriceSection').addClass("has-success");
+			}
+
+			// if an item year is not given
+			if(itemYearField == ""){
+				// give input box a red border
+				$('#itemYearSection').addClass("has-error");
+				// Error message
+				$("#yearValueError").text("Please provide the year of purchase");
+			}
+			else{
+				$('#itemYearSection').addClass("has-success");
+			}
+
+			// if a picture of the item is not given
+			if(itemPictureField == ""){
+				// give input box a red border
+				$('#itemPictureSection').addClass("has-error");
+				// Error message
+				$('#pictureOfItemError').text("Please provide a picture of the item");
+			}
+			else{
+				// give input box a green border
+				$('#itemPictureSection').addClass("has-success");
+			}
+
+			// Please provide a receipt of the item
+			if(itemReceiptField == ""){
+				// give input box a red border
+				$("#itemReceiptSection").addClass("has-error");
+				// Error message
+				$("#pictureOfReceiptError").text("Please provide a picture of the receipt of the item");
+			}
+			else{
+				// give input box a green border
+				$("#itemReceiptSection").addClass("has-success");
+			}
+
+			if(itemManufacturerField == ""){
+				// give input box a red border
+				$("#itemManufacturerSection").addClass("has-error");
+				// Error message
+				$("#manufacturerNameError").text("Please provide the item's manufacturer");
+			}
+			else{
+				// give input box a green border
+				$("#itemManufacturerSection").addClass("has-success");
+			}	
 		}
 
-		// if an item price is not given
-		if(itemPriceField == ""){
-			// Give input box a red border
-			$('#itemPriceSection').addClass("has-error");
-			// Error message
-			$("#priceValueError").text("Please provide the item's price");
-		}
-		else{
-			$('#itemPriceSection').addClass("has-success");
-		}
-
-		// if an item year is not given
-		if(itemYearField == ""){
-			// give input box a red border
-			$('#itemYearSection').addClass("has-error");
-			// Error message
-			$("#yearValueError").text("Please provide the year of purchase");
-		}
-		else{
-			$('#itemYearSection').addClass("has-success");
-		}
-
-		// if a picture of the item is not given
-		if(itemPictureField == ""){
-			// give input box a red border
-			$('#itemPictureSection').addClass("has-error");
-			// Error message
-			$('#pictureOfItemError').text("Please provide a picture of the item");
-		}
-		else{
-			// give input box a green border
-			$('#itemPictureSection').addClass("has-success");
-		}
-
-		// Please provide a receipt of the item
-		if(itemReceiptField == ""){
-			// give input box a red border
-			$("#itemReceiptSection").addClass("has-error");
-			// Error message
-			$("#pictureOfReceiptError").text("Please provide a picture of the receipt of the item");
-		}
-		else{
-			// give input box a green border
-			$("#itemReceiptSection").addClass("has-success");
-		}
-
-		if(itemManufacturerField == ""){
-			// give input box a red border
-			$("#itemManufacturerSection").addClass("has-error");
-			// Error message
-			$("#manufacturerNameError").text("Please provide the item's manufacturer");
-		}
-		else{
-			$("#itemManufacturerSection").addClass("has-success");
-		}
 
 	});
 });
